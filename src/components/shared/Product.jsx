934c885baf78aa44edd7b432d488eadf4f216fb7
Taufiq720 <taufiq.r360@gmail.com>
2026-07-18 23:44:23 +0600

    Product Page Animation and Formatting

--- src/components/shared/Product.jsx
+++ src/components/shared/Product.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 
 const Product = ({ product }) => {
-  const { image, title, description, tags, isNew } = product;
+  const { image, title, price, description, tags, isNew } = product;
 
   return (
     <div className="card bg-base-100 w-full max-w-sm shadow-sm border border-base-200">
-      <figure className="px-4 pt-4">
-        <img
-          src={image || "https://daisyui.com"}
-          alt={title}
-          className="rounded-xl h-48 w-full object-contain bg-base-200"
-        />
-      </figure>
+      <figure className="overflow-hidden rounded-t-xl">
+  <img
+    src={image}
+    alt={title}
+    className="w-full aspect-25/26 object-cover transition-transform duration-500 hover:scale-130"
+  />
+</figure>
       <div className="card-body">
         <h2 className="card-title text-base-content">
           {title}
           {isNew && <div className="badge badge-secondary text-xs">NEW</div>}
         </h2>
+        <p className='text-xl font-bold'>৳{price}</p>
         <p className="text-sm text-base-content/70">{description}</p>
         <div className="card-actions justify-end mt-4">
           {tags?.map((tag, index) => (
